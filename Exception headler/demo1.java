0fa580367bfc92e785a0f4295a11586c02f14a41
Gautam Krishna M <charmingdeveloper@protonmail.com>
2021-11-06 20:20:10 +0530

    ,nn,

---
+++ Exception headler/demo1.java
@@ -0,0 +1,15 @@
+
+/**
+ * demo1
+ */
+public class demo1 {
+
+
+    public static void main(String[] args) {
+        division(100, 12);
+    }
+    public static void division(int total_sum,int total_people){
+        int result=total_sum/total_people;
+        System.out.println("The result is  : "+result);
+    }
+}
