b190559dfa7f82bc53340b2da34f08bbb9f120d7
Gautam Krishna M <charmingdeveloper@protonmail.com>
2021-12-04 22:39:47 +0530

    sdgsg

--- java_class_2/graphics/src/graphics/demo.java
+++ java_class_2/graphics/src/graphics/demo.java
@@ -1,1 +1,33 @@
+package graphics;
 
+import java.sql.*;
+
+public class demo {
+    public static void main(String[] args) {
+        String[] name = new String[3];
+        int i = 0;
+
+        Connection c = null;
+        Statement stmt = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/help", "postgres", "Gautam3377");
+            stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery("select* from login;");
+            while (rs.next()) {
+                name[i] = rs.getString("username");
+                i++;
+            }
+            rs.close();
+            stmt.close();
+            c.close();
+
+            System.out.println("Opened database successfully");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+
+    }
+}
--- java_class_2/graphics/src/learning/t_1.java
+++
@@ -1,8 +0,0 @@
-package learning;
-
-public class t_1 {
-    public static void main(String[] args) {
-        System.out.println("hello");
-
-    }
-}
