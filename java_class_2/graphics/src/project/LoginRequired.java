32dd99a5195a14225ff71f4d6120e0afd3afbfe9
Gautam Krishna M <charmingdeveloper@protonmail.com>
2021-12-09 12:02:53 +0530

    bn

---
+++ java_class_2/graphics/src/project/LoginRequired.java
@@ -0,0 +1,47 @@
+package project;
+
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class LoginRequired {
+
+    LoginRequired() {
+        JFrame f = new JFrame("Login Required");
+        f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        f.setResizable(false);
+        f.setSize(400, 300); // not recommended, but used here for convenience
+        f.setLocationByPlatform(true);
+        f.setVisible(true);
+
+        showLogin(f);
+    }
+
+    public void showLogin(JFrame frame) {
+        JPanel p = new JPanel(new BorderLayout(5, 5));
+
+        JPanel labels = new JPanel(new GridLayout(0, 1, 2, 2));
+        labels.add(new JLabel("User Name", SwingConstants.TRAILING));
+        labels.add(new JLabel("Password", SwingConstants.TRAILING));
+        p.add(labels, BorderLayout.LINE_START);
+
+        JPanel controls = new JPanel(new GridLayout(0, 1, 2, 2));
+        JTextField username = new JTextField("Joe Blogs");
+        controls.add(username);
+        JPasswordField password = new JPasswordField();
+        controls.add(password);
+        p.add(controls, BorderLayout.CENTER);
+
+        JOptionPane.showMessageDialog(
+                frame, p, "Log In", JOptionPane.QUESTION_MESSAGE);
+        System.out.println("User Name: " + username.getText());
+        System.out.println("Password: " + new String(password.getPassword()));
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new LoginRequired();
+        });
+    }
+}
