32cb230636efa9e0744826e1861dc4513af5d214
Gautam Krishna M <charmingdeveloper@protonmail.com>
2021-12-14 20:59:08 +0530

    12

--- java_class_2/graphics/src/project/Client_info.java
+++ java_class_2/graphics/src/project/Client_info.java
@@ -1,6 +1,5 @@
 package project;
 
-
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -25,6 +24,7 @@ public class Client_info extends JFrame {
 	private JPanel contentPane;
 	private JTextField textField;
 	private JTable table;
+	JDBC_table_printer table_make = new JDBC_table_printer();
 
 	/**
 	 * Launch the application.
@@ -52,13 +52,13 @@ public class Client_info extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JPanel Header = new JPanel();
 		Header.setLayout(null);
 		Header.setBackground(Color.BLACK);
 		Header.setBounds(0, 0, 784, 68);
 		contentPane.add(Header);
-		
+
 		JLabel heading = new JLabel("MyPharma");
 		heading.setHorizontalAlignment(SwingConstants.CENTER);
 		heading.setForeground(Color.WHITE);
@@ -66,14 +66,14 @@ public class Client_info extends JFrame {
 		heading.setBackground(Color.WHITE);
 		heading.setBounds(164, 0, 436, 58);
 		Header.add(heading);
-		
+
 		JPanel Nav = new JPanel();
 		Nav.setLayout(null);
 		Nav.setForeground(Color.WHITE);
 		Nav.setBackground(Color.PINK);
 		Nav.setBounds(0, 66, 784, 74);
 		contentPane.add(Nav);
-		
+
 		JButton Nav_btn2 = new JButton("Medicine Info");
 		Nav_btn2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -84,7 +84,7 @@ public class Client_info extends JFrame {
 		});
 		Nav_btn2.setBounds(292, 25, 165, 39);
 		Nav.add(Nav_btn2);
-		
+
 		JButton Nav_btn3 = new JButton("Profile");
 		Nav_btn3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -95,49 +95,40 @@ public class Client_info extends JFrame {
 		});
 		Nav_btn3.setBounds(519, 25, 165, 39);
 		Nav.add(Nav_btn3);
-		
+
 		Button Nav_btn4 = new Button("Client info ");
 		Nav_btn4.setBackground(new Color(0, 206, 209));
 		Nav_btn4.setBounds(65, 25, 165, 39);
 		Nav.add(Nav_btn4);
-		
+
 		textField = new JTextField();
 		textField.setBounds(318, 35, -109, 241);
 		Nav.add(textField);
 		textField.setColumns(10);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(75, 168, 602, 182);
+		scrollPane.setBounds(10, 150, 766, 268);
 		contentPane.add(scrollPane);
-		
+
 		table = new JTable();
 		scrollPane.setViewportView(table);
-		
+
+		table_make.print_table("client", "mypharma", table);
+
 		JPanel panel = new JPanel();
 		panel.setBounds(118, 428, 530, 49);
 		contentPane.add(panel);
-		
+
 		JButton btnNewButton_2 = new JButton("Delete");
 		btnNewButton_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				table_make.delete_box("client", table, "mypharma", "client_id");
+				table_make.print_table("client", "mypharma", table);
 			}
 		});
 		panel.add(btnNewButton_2);
-		
+
 		Component horizontalStrut = Box.createHorizontalStrut(40);
 		panel.add(horizontalStrut);
-		
-		JButton btnNewButton_1 = new JButton("New button");
-		panel.add(btnNewButton_1);
-		
-		JButton btnNewButton = new JButton("New button");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		
-		Component horizontalStrut_1 = Box.createHorizontalStrut(40);
-		panel.add(horizontalStrut_1);
-		panel.add(btnNewButton);
 	}
 }
--- java_class_2/graphics/src/project/Client_login_connector.java
+++ java_class_2/graphics/src/project/Client_login_connector.java
@@ -4,19 +4,47 @@ import java.sql.*;
 
 public class Client_login_connector {
     boolean checker;
+    String id;
 
-    public void linker(String name, int pass) {
+    public void regsql(int client_id, String usrnam, String password, String client_address, String client_name,
+            String client_email, String mobilephone) {
+        String jdbcURL = "jdbc:postgresql://localhost:5432/mypharma";
+        String username = "postgres";
+        String password1 = "admin";
+
+        try {
+            Connection connection = DriverManager.getConnection(jdbcURL, username, password1);
+            PreparedStatement s1 = connection.prepareStatement("select count(client_id) AS rowcount from client");
+            ResultSet rs = s1.executeQuery();
+            rs.next();
+            client_id = client_id + rs.getInt("rowcount");
+
+            String s = "INSERT into  client values (" + client_id + "," + "'" + usrnam + "'" + "," + "'" + password
+                    + "'" + "," + "'" + client_address + "'" + "," + "'" + client_name + "'" + "," + "'" + client_email
+                    + "'" + ","
+                    + "'" + mobilephone + "'" + ");";
+            PreparedStatement ps2 = connection.prepareStatement(s);
+            ps2.executeQuery();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void linker(String name, String pass, String tablename, String data_base) {
         Connection con = null;
         Statement stmt = null;
         try {
             Class.forName("org.postgresql.Driver");
-            con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/help", "postgres", "Gautam3377");
+            con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/" + data_base, "postgres",
+                    "admin");
             stmt = con.createStatement();
-            String sql = "select * from login where username = '" + name + "' and password =" + pass + ";";
+            String sql = "select * from " + tablename + " where username = '" + name + "' and password = '" + pass
+                    + "' ;";
             ResultSet res = stmt.executeQuery(sql);
             if (res.next()) {
                 System.out.println("Yeah");
                 checker = true;
+                id = res.getString(1);
 
             } else {
                 System.out.println("oobi");
@@ -35,4 +63,5 @@ public class Client_login_connector {
         }
 
     }
+
 }
