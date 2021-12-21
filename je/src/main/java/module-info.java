757cf25264cf7c0bb47f7dd2bb3621331d8dee6d
Gautam Krishna M <charmingdeveloper@protonmail.com>
2021-12-21 22:51:19 +0530

    dfg

--- je/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.je {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.je to javafx.fxml;
-    exports com.example.je;
-}
--- src/nyrwana/p1/part2.java
+++
@@ -1,7 +0,0 @@
-package nyrwana.p1;
-
-public class part2 {
-    public void display(){
-        System.out.println("Hellow world from nyrwana part 2");
-    }
-}
